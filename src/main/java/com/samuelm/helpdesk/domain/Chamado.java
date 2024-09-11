9f5611c386809fde494916447d2263aebed10db6
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-10 22:38:37 -0300

    implemented jpa annotations

--- src/main/java/com/samuelm/helpdesk/domain/Chamado.java
+++ src/main/java/com/samuelm/helpdesk/domain/Chamado.java
@@ -1,21 +1,33 @@
 package com.samuelm.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.samuelm.helpdesk.domain.enums.Prioridade;
 import com.samuelm.helpdesk.domain.enums.Status;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento ;
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observacoes;
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado(){
--- src/main/java/com/samuelm/helpdesk/domain/Pessoa.java
+++ src/main/java/com/samuelm/helpdesk/domain/Pessoa.java
@@ -1,21 +1,32 @@
 package com.samuelm.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.samuelm.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public abstract class Pessoa {
+@Entity
+public abstract class Pessoa implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
     protected String nome;
+    @Column(unique = true)
     protected String cpf;
+    @Column(unique = true)
     protected String email;
     protected String senha;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
     protected Set<Integer> perfis = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     protected LocalDate dataCriacao = LocalDate.now();
 
     public Pessoa(){

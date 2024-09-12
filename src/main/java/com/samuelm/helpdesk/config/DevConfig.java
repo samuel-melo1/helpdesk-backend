163c6bb685d2d12a33ef32cef77206203307ac7e
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-11 22:43:17 -0300

    development perfil created

---
+++ src/main/java/com/samuelm/helpdesk/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.samuelm.helpdesk.config;
+
+import com.samuelm.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService service;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public boolean instanciaDB(){
+        if(value.equals("create")){
+            this.service.instaciaDB();
+        }
+        return false;
+    }
+}

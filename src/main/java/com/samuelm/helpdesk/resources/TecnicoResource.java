9a90f114104796921f5f04c5c379acfc94b7664b
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-11 23:14:42 -0300

    pattern dto to tecnico

--- src/main/java/com/samuelm/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/samuelm/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.samuelm.helpdesk.resources;
 
 import com.samuelm.helpdesk.domain.Tecnico;
+import com.samuelm.helpdesk.dtos.TecnicoDTO;
 import com.samuelm.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ public class TecnicoResource {
     @Autowired
     private TecnicoService service;
     @GetMapping("/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable("id") Integer id){
-        return ResponseEntity.ok(service.findById(id));
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable("id") Integer id){
+        return ResponseEntity.ok(new TecnicoDTO(service.findById(id)));
     }
 }

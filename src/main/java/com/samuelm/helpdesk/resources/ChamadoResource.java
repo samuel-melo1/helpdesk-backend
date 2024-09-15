454de1402fc36422f222ee33edb1506099b72b59
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-14 23:53:29 -0300

    implemented find by id to chamado

---
+++ src/main/java/com/samuelm/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,22 @@
+package com.samuelm.helpdesk.resources;
+
+import com.samuelm.helpdesk.dtos.ChamadoDTO;
+import com.samuelm.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+    @GetMapping("/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable("id") Integer id){
+        return  ResponseEntity.ok().body(new ChamadoDTO(service.findById(id)));
+    }
+}

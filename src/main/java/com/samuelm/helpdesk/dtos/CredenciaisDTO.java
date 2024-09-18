427269cd7ccdee4bcdc1ef1bde9ea0f22a29e3d6
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-09-17 22:59:40 -0300

    implemented dto to credentials and user details

---
+++ src/main/java/com/samuelm/helpdesk/dtos/CredenciaisDTO.java
@@ -0,0 +1,25 @@
+package com.samuelm.helpdesk.dtos;
+
+public class CredenciaisDTO {
+
+    private String email;
+    private String senha;
+
+    public CredenciaisDTO(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}

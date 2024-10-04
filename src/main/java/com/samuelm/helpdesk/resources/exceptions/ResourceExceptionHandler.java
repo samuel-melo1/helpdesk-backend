7445632c5647d1e88bbe7ac096150012a7809b97
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-10-03 23:27:19 -0300

    validation cpf in exception handler

--- src/main/java/com/samuelm/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/samuelm/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.samuelm.helpdesk.resources.exceptions;
 import com.samuelm.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.samuelm.helpdesk.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -45,4 +47,16 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> validationErrors(ConstraintViolationException exception,
+                                                          HttpServletRequest request){
+        ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Validation Error", "Erro na validação dos campos", request.getRequestURI());
+
+        for(ConstraintViolation field : exception.getConstraintViolations()){
+            errors.addError(field.getPropertyPath().toString(), field.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
 }

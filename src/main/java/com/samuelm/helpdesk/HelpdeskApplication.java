1b305491d58f9492026c1a0d119a39f33eeea390
samuel-melo1 <samuelprogramacao3@gmail.com>
2024-10-03 23:45:53 -0300

    define locale pt/br to railway deployment

--- src/main/java/com/samuelm/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/samuelm/helpdesk/HelpdeskApplication.java
@@ -2,9 +2,13 @@ package com.samuelm.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+
 @SpringBootApplication
 public class HelpdeskApplication   {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
         SpringApplication.run(HelpdeskApplication.class, args);
     }
 }
